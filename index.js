cee5e143a62c08fb403febcf5ae0a7395868758b
Ian Greenblott <imgreenblott@gmail.com>
2021-11-02 18:06:55 -0400

    Added comments for later.

---
+++ index.js
@@ -0,0 +1,8 @@
+//Grab all the classses , inquirer , pATH , generateHTML 
+
+//create an init() 
+//welcome message 
+//first ask questions related to Manager ex: Please enter {id, name , email and officenumber } 
+// then question with type list : Choose 'Engineer' or Intern or 'None
+//based on th option ask four question related to the class 
+// but when the user selects NONE - generate HTML Page 
