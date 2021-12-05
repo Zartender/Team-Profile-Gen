89b25f3d001f28a06931a6c8b5b376913f89b734
Ian Greenblott <imgreenblott@gmail.com>
2021-12-05 12:35:15 -0500

    Adding inquirer prompt questions to index.js

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 //Grab all the classses , inquirer , pATH , generateHTML 
-const Manager = require("./lib/Manager");
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-constIntern = require("./lib/Intern");
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const team = [];
+const ID = [];
+// loads template file
+const templater = require('./templater')
+
+
+
+inquirer
+    .prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'Provide your first and last name.',
+            
+        },
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Provide your ID number.',
+            
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'Provide your email.',
+            
+        },
+        {
+            name: 'office',
+            type: 'input',
+            message: 'Please provide your office number.',
+            
+        },
+       
+    ])
+
 
 //create an init() 
 //welcome message 
