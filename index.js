f07474a4ef7791c43921a76f3284eb68db47b528
Ian Greenblott <imgreenblott@gmail.com>
2021-12-05 13:56:12 -0500

    Completed questiosn for inquirer.

--- index.js
+++ index.js
@@ -9,52 +9,136 @@ const path = require('path');
 const fs = require('fs');
 
 const team = [];
-const ID = [];
-// loads template file
-const templater = require('./templater')
+const ID= [];
 
-
-// questions prompted via inquirer
+// initial quesitons
 inquirer
     .prompt([
         {
             name: 'name',
             type: 'input',
-            message: 'Provide your first and last name.',
-            
+            message: 'Enter your name',
         },
         {
             name: 'id',
             type: 'input',
-            message: 'Provide your ID number.',
-            
+            message: 'Enter your ID number',
         },
         {
             name: 'email',
             type: 'input',
-            message: 'Provide your email.',
-            
+            message: 'Provide your email',
         },
         {
-            name: 'office',
+            name: 'officeNumber',
             type: 'input',
-            message: 'Please provide your office number.',
-            
+            message: 'What is your office number',
         },
-       
+        
     ])
-.then((answers) => {
-    console.log(answers);
-    const newManager = new Manager(answers.name, answers.id, answers.email, answers.office);
-    team.push(newManager);
-    ID.push(answers.managerID);
-});
+    .then((answers) => {
+        console.log(answers)
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        team.push(manager);
+        idArray.push(answers.managerId)
+        menu();
+    });
+// adding another team member
+function menu() {
+    inquirer
+        .prompt([
+            {
+                name: 'team',
+                type: 'list',
+                choices: ['Engineer', 'Intern', 'Finish'],
+                message: 'Would you like to add another team member?',
+            },
+        ])
+        .then(userchoice => {
+            console.log(userchoice);
+            switch (userchoice.team) {
+                case 'Engineer':
+                    engineerQues();
+                    break;
+                case 'Intern':
+                    internQues();
+                default:
+                    buildTeam(teamList);
+            }
 
+        })
+}
+// Adding an engineer
+function engineerQues() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: 'What is the name of your employee?',
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: 'What is the id number of your employee?',
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: 'What is the email for your employee?',
+            },
+            {
+                name: 'github',
+                type: 'input',
+                message: 'What is the GitHub username for your employee?',
+            },
+        ])
+        .then((answers) => {
+            console.log(answers)
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            teamList.push(engineer);
+            idArray.push(answers.engineerId);
+            menu();
+        })
+    }
 
+function internQues() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: 'What is the name of your employee?',
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: 'What is the id number of your employee?',
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: 'What is the email for your employee?',
+            },
+            {
+                name: 'education',
+                type: 'input',
+                message: 'Does the intern have a GED or highschool deploma, or a bachelors dehgree?',
+            },
+        ])
+        .then((answers) => {
+            console.log(answers)
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            teamList.push(intern);
+            idArray.push(answers.internId);
+            menu();
+        })
+}
 
-//create an init() 
-//welcome message 
-//first ask questions related to Manager ex: Please enter {id, name , email and officenumber } 
-// then question with type list : Choose 'Engineer' or Intern or 'None
-//based on th option ask four question related to the class 
-// but when the user selects NONE - generate HTML Page 
+function buildTeam(team) {
+    console.log(team);
+    const generateTemplate = template(team);
+    fs.writeFile('index.html', generateTemplate, 'utf-8', (err) => err ?
+        console.log(err) :
+        console.log('Task Completed.'))
+}                  
